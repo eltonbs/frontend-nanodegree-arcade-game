954f325d24c86b10787954e85230be3c6d3f1c27
Elton Sousa <eltonbsousa@gmail.com>
2017-07-27 19:04:43 -0400

    Add the player and the enemies

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// Size of the "grid"
+var numRows  = 6;
+var numCols = 5;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,6 +10,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.reset();
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,13 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.speed * dt);
+
+    // If the enemy reaches the end of canvas, 
+    // put it back at the beginning at a random row and speed
+    if (this.x > ctx.canvas.clientWidth) {
+        this.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +34,76 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+Enemy.prototype.reset = function() {
+    // Set the initial enemy's position
+    this.x = -100;
+    this.row = getRandomInt(1,3);
+    this.y = this.row * 83 -20;
+
+    // Initial enemy's speed
+    this.speed = getRandomInt(100,400);
+};
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    this.sprite = 'images/char-boy.png';
+    this.reset();
+};
 
+// Update the player's position
+Player.prototype.update = function() {
+    this.x = this.col * 101;
+    this.y = this.row * 83 -12;
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.reset = function() {
+    // Set the initial player's position
+    this.row = 5;
+    this.col = 2;
+};
+
+// Handle the keyborard input
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+        case 'up':
+            if (this.row > 0) {
+                this.row--;
+            }
+            break;
+        case 'down':
+            if (this.row < numRows -1) {
+                this.row++;
+            }
+            break;
+        case 'left':
+            if (this.col > 0) {
+                this.col--;
+            }
+            break;
+        case 'right':
+            if (this.col < numCols -1) {
+                this.col++;
+            }
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [];
+for (var i = 0; i < 4; i++) {
+    allEnemies.push(new Enemy);
+}
 
-
+var player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +117,10 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// Get a random integer between min and max (both inclusive)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
